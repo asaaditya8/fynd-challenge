32f330e0fe58f60dfc13dcb1d08b159433aec163
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2019-03-27 22:48:32 +0530

    Scripting

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,8 +5,11 @@ from torchvision.transforms import transforms
 
 def main():
     #inception_v3 requires the input size to be (299,299), whereas all of the other models expect (224,224).
+    # The images have to be loaded in to a range of [0, 1] and then normalized
     tfms = transforms.Compose([
-        transforms.CenterCrop(224)
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
     path = os.path.abspath('data/raw/images/Train Directory/')
@@ -15,7 +18,7 @@ def main():
               os.walk(os.path.abspath('data/raw/images/Train Directory')) for name in files]
 
     data = ImageDataBunch.from_name_func(path=path, fnames=fnames, label_func=lambda x: x.split('/')[-2],
-                                         valid_pct=0.6, test=testpath, ds_tfms=(tfms, []))
+                                         valid_pct=0.6, test=testpath, ds_tfms=(tfms, tfms))
 
     print(data.classes)
 
