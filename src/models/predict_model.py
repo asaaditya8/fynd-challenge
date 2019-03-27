4d0a38cfe5f6f3ff8e054693bc244a74136fb1c5
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2019-03-27 22:27:29 +0530

    Writing scripts

---
+++ src/models/predict_model.py
@@ -0,0 +1,28 @@
+import fastai
+from fastai.vision import *
+from torchvision.transforms import transforms
+
+
+def main():
+    #inception_v3 requires the input size to be (299,299), whereas all of the other models expect (224,224).
+    tfms = transforms.Compose([
+        transforms.CenterCrop(224)
+    ])
+
+    path = os.path.abspath('data/raw/images/Train Directory/')
+    testpath = os.path.abspath('data/raw/images/Test Directory/')
+    fnames = [os.path.join(path, name) for path, subdirs, files in
+              os.walk(os.path.abspath('data/raw/images/Train Directory')) for name in files]
+
+    data = ImageDataBunch.from_name_func(path=path, fnames=fnames, label_func=lambda x: x.split('/')[-2],
+                                         valid_pct=0.6, test=testpath, ds_tfms=tfms)
+
+    print(data.classes)
+
+    ds = data.train_ds
+
+    #visualise samples
+    data.show_batch(rows=3, figsize=(5, 5))
+
+if __name__ == '__main__':
+    main()
--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,4 +1,4 @@
 from src.data import make_dataset
 
 def test_extract():
-    assert make_dataset.extract_tar('data/raw/images.tar.gz', 'data/raw') != 0
+    assert make_dataset.extract_tar('data/raw/images.tar.gz', 'data/raw/') != 0
