c793c568c754247e92cf412fd7a1ae5c39b54359
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2019-03-29 00:39:17 +0530

    Added print statements in make_dataset.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -44,7 +44,7 @@ def split_folder(imgdir, outdir, val_num:int = 100, test_size:float = 0.2, shuff
     all_img_filenames = np.array([os.path.join(path, name) for path, subdirs, files in
                                   os.walk(os.path.abspath(imgdir)) for name in files])
 
-    print(len(all_img_filenames), 'images found.')
+    print(len(all_img_filenames), 'images found belonging to', len(os.listdir(imgdir)), 'classes.')
 
     if stratify:
         label_fn = lambda x: x.split('/')[-2]
@@ -68,6 +68,8 @@ def split_folder(imgdir, outdir, val_num:int = 100, test_size:float = 0.2, shuff
                                           shuffle=shuffle)
 
     var_dict = {'train': train_names, 'valid': valid_names, 'test': test_names}
+
+    for v in var_dict: print(len(var_dict[v]), v, 'images')
 
     for f in tqdm(var_dict):
         folder = os.path.join(os.path.abspath(outdir), f)
