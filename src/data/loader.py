1720ebf58d6c5c3c6ca2552cfc84719268c50e9f
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2019-03-30 20:55:11 +0530

    Fixing loader.py.

--- src/data/loader.py
+++ src/data/loader.py
@@ -5,11 +5,11 @@ import os
 BATCH_SIZE = 32
 
 def get_data(inp_dir):
-    aug = ImageDataGenerator(preprocessing_function=preprocess_input)
     var_list = ('train', 'valid', 'test')
     var_dict ={}
     for v in var_list:
-        var_dict[v] = aug.flow_from_directory(os.path.join(os.path.abspath(inp_dir), v),
+        var_dict[v] = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(
+                                              os.path.join(os.path.abspath(inp_dir), v),
                                               batch_size=BATCH_SIZE,
                                               target_size=(224,224))
     return var_dict
