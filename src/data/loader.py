55176579bfab9137a84c0044b358718097c02df6
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2019-04-02 23:05:07 +0530

    Experiment 9 fixed loader.

--- src/data/loader.py
+++ src/data/loader.py
@@ -8,10 +8,16 @@ def get_data(inp_dir):
     var_list = ('train', 'valid', 'test')
     var_dict ={}
     for v in var_list:
-        var_dict[v] = ImageDataGenerator(brightness_range=(0,0.1),
-                                         channel_shift_range=0.1,
-            preprocessing_function=preprocess_input).flow_from_directory(
-                                              os.path.join(os.path.abspath(inp_dir), v),
-                                              batch_size=BATCH_SIZE,
-                                              target_size=(224,224))
+        if v == 'train':
+            var_dict[v] = ImageDataGenerator(brightness_range=(0,0.1),
+                                             channel_shift_range=0.1,
+                preprocessing_function=preprocess_input).flow_from_directory(
+                                                  os.path.join(os.path.abspath(inp_dir), v),
+                                                  batch_size=BATCH_SIZE,
+                                                  target_size=(224,224))
+        else:
+            var_dict[v] = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(
+                                                    os.path.join(os.path.abspath(inp_dir), v),
+                                                    batch_size=BATCH_SIZE,
+                                                    target_size=(224, 224))
     return var_dict
